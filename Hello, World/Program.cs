b7bff41451c061f720b89df9b1b0a000616b8477
Petr Kamarýt <petr.kamaryt@thermofisher.com>
2025-04-17 18:38:51 +0200

    Initial commit

--- Hello, World/Program.cs
+++ Hello, World/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! 😎");
+    }
+}
